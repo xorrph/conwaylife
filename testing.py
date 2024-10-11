1e408fecd59fa070fda28eecedf70c0f265f4842
xorrph <130938207+xorrph@users.noreply.github.com>
2024-10-11 22:09:25 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,17 +1,20 @@
+import time
+import random
+
 x = 1
 y = 1
-width = 3
-height = 3
-g = [[0 for a in range(width)] for b in range(height)]
-g[0][1] = 1
-g[2][1] = 1
-g[1][1] = 1
-g[2][2] = 1
-g[0][0] = 1
-for r in g: print(r)
+width = 4
+height = 4
 
-def updateBoard(g,x,y,width,height):
-    newG = g.copy()
+g = [[0 for a in range(width)] for b in range(height)]
+for r in g:
+    for cell in range(0,len(r)):
+        if random.randint(0,1) ==  1:
+            r[cell] = 1
+    print (r)
+            
+
+def updateBoard(g,x,y,width,height,newG):
     livecount = checkNeighbours(g,x,y,width,height)
     print(livecount)
     if livecount < 2:
@@ -21,6 +24,19 @@ def updateBoard(g,x,y,width,height):
     elif livecount == 3:
         newG[x][y] = 1
     return newG
+
+def runGame(g,x,y,width,height):
+    userinp = input("enter 0 to end the game")
+    while userinp != "0":
+        newG = g.copy()
+        for x in range(width):
+            for y in range(height):
+                newG = updateBoard(g,x,y,width,height,newG)
+        for r in newG:print(r)
+        userinp = input("enter 0 to end the game")
+        g = newG.copy()
+                
+
 def checkNeighbours(g,x,y,width,height):
         livecount = 0
         if x == 0 and y == 0: # top left corner
@@ -31,7 +47,7 @@ def checkNeighbours(g,x,y,width,height):
                         if i == x and j == y:
                             continue
                         livecount += 1
-        elif x == 0 and y == height: # bottom left corner
+        elif x == 0 and y == height - 1: # bottom left corner
             print("checkq")
             for i in range(x, x + 2):
                 for j in range(y - 1, y + 1):
@@ -39,7 +55,7 @@ def checkNeighbours(g,x,y,width,height):
                         if i == x and j == y:
                             continue
                         livecount += 1
-        elif x == width and y == height: # bottom right corner
+        elif x == width -1 and y == height -1: # bottom right corner
             print("check3")
             for i in range(x - 1, x + 1):
                 for j in range(y - 1, y + 1):
@@ -47,7 +63,7 @@ def checkNeighbours(g,x,y,width,height):
                         if i == x and j == y:
                             continue
                         livecount += 1
-        elif x == width and y == 0: # top right corner
+        elif x == width -1 and y == 0: # top right corner
             print("check4")
             for i in range(x, x + 1):
                 for j in range(y, y + 2):
@@ -63,7 +79,7 @@ def checkNeighbours(g,x,y,width,height):
                         if i == x and j == y:
                             continue
                         livecount += 1
-        elif x == width: #right side
+        elif x == width -1 : #right side
             print("check6")
             for i in range(x-1, x + 1):
                 for j in range(y - 1, y + 2):
@@ -71,7 +87,7 @@ def checkNeighbours(g,x,y,width,height):
                         if i == x and j == y:
                             continue
                         livecount += 1
-        elif y == height: #bottom side
+        elif y == height -1 : #bottom side
             print("check7")
             for i in range(x-1, x + 2):
                 for j in range(y - 1, y + 1):
@@ -83,6 +99,8 @@ def checkNeighbours(g,x,y,width,height):
             print("check8")
             for i in range(x-1, x + 2):
                 for j in range(y, y + 2):
+                    print("x",x,y)
+                    print(i,j)
                     if g[i][j] == 1:
                         if i == x and j == y:
                             continue
@@ -98,7 +116,7 @@ def checkNeighbours(g,x,y,width,height):
         return livecount
 
 
-updateBoard(g,x,y,width,height)
+runGame(g,x,y,width,height)
 print()
 print()
 for r in g: print(r)
